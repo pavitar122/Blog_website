3b9c8474ca6c4ebe5d129725e0ea913f314d777e
pavitar122 <pavitar3335@gmail.com>
2023-07-25 15:23:13 +0530

    Database

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from forms import Register_form
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "dgiufhuhewhfuwhfuwbubwugbw"
 Bootstrap(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE")
+# postgresql://database_zyph_user:cTovrrBF4vyr8gm3PdxWglnpeE8xVZhr@dpg-civpcdunqql48o1dt8o0-a.oregon-postgres.render.com/database_zyph
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class New_user(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
 
 
 @app.route("/")
@@ -12,9 +27,18 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = Register_form()
+    if form.validate_on_submit():
+        add_user = New_user(
+            email=form.email.data,
+            password=form.password.data,
+            name=form.name.data
+        )
+        db.session.add(add_user)
+        db.session.commit()
+        return redirect(url_for("home"))
     return render_template("register.html", form=form)
 
 
